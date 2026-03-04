5b69d686fa6cac7dfe8b40baf7145780a3569a2c
jurabekkamolovdev <jurabek.dev101@gmail.com>
2026-03-05 01:37:56 +0500

    [+]

--- vue/CDN/app.js
+++ vue/CDN/app.js
@@ -6,28 +6,56 @@ const App = {
             inputValue: '',
             notes: ['1-Task', '2-Task']
             // counter: 0,
-        }
+        };
     },
 
     methods: {
         inputChangeHandler(event) {
-            this.inputValue = event.target.value
+            this.inputValue = event.target.value;
         },
 
         addHandler() {
-            if (this.inputValue !== '') this.notes.push(this.inputValue)
-            this.inputValue = ''
+            if (this.inputValue !== '') this.notes.push(this.inputValue);
+            this.inputValue = '';
         },
 
         popHandler() {
-            this.notes.pop()
+            this.notes.pop();
         },
-        
+
+        toUpperCase(item) {
+            return item.toUpperCase();
+        },
+
         deleteHandler(index) {
-            this.notes.splice(index, 1)
+            this.notes.splice(index, 1);
+        },
+
+        keypressHandler(event) {
+            if (event.key === 'Enter') {
+                this.addHandler();
+            }
+        },
+
+        // doubleCount() {
+        //     console.log('dCount');
+        //     return this.notes.length * 2
+        // }
+        
+    },
+
+    computed: {
+        doubleCount() {
+            console.log('dCount');
+            return this.notes.length * 2
+        }
+    },
+
+    watch: {
+        inputValue(value) {
+            console.log('Watch', value)
         }
     }
-}
+};
 
-Vue.createApp(App).mount("#app")
-
+Vue.createApp(App).mount('#app')

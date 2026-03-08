3df508bfc45bc9687a9e920cd53f21abc1a31d47
jurabekkamolovdev <jurabek.dev101@gmail.com>
2026-03-08 07:10:44 +0500

    [+] components

---
+++ vue/components/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h2>So'ngi yangiliklar {{ now }}</h2>
+    </div>
+
+    <AppNews
+      v-for="item in news"
+      v-bind:key="item.id"
+      v-bind:title="item.title"
+      v-bind:desc="item.desc"
+    />
+
+  </div>
+</template>
+
+<script>
+import AppNews from './AppNews.vue'
+export default {
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        {
+          id: 1,
+          title: 'Bugun ramazonning 17-kuni',
+          desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis consequuntur accusantium cumque veritatis distinctio porro.'
+        },
+        {
+          id: 2,
+          title: 'Bugun xotin-qizlar bayrami',
+          desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis consequuntur accusantium cumque veritatis distinctio porro.'
+        }
+      ]
+    }
+  },
+  components: {
+    AppNews
+  }
+}
+</script>
+
+<style></style>
---
+++ vue/components/src/AppNews.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+
+    <button class="btn" v-on:click="isOpen = !isOpen">Ko'rish</button>
+
+    <p v-if="isOpen">{{ desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    desc: String
+  },
+
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>

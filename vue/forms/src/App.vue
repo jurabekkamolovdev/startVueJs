570d23559a019edb007310a52ae9940341399779
jurabekkamolovdev <jurabek.dev101@gmail.com>
2026-03-19 19:36:22 +0500

    []

---
+++ vue/forms/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <form action="" class="card">
+      <h1>Anketa Vue uchun !</h1>
+      <div class="form-control">
+        <label for="name">Ismingizni nima ?</label>
+        <input type="text" id="name" placeholder="Ismingizni yozing">
+      </div>
+
+      <div class="form-control">
+        <label for="age"></label>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>

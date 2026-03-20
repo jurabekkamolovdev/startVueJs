d06df18ac0f042be0c31bc88156b005709d46c66
jurabekkamolovdev <jurabek.dev101@gmail.com>
2026-03-20 12:02:35 +0500

    [+] formt style and html

--- vue/forms/src/App.vue
+++ vue/forms/src/App.vue
@@ -8,8 +8,52 @@
       </div>
 
       <div class="form-control">
-        <label for="age"></label>
+        <label for="age">Yoshingiz nechida ?</label>
+        <input type="number" id="age" value="20">
       </div>
+
+      <div class="form-control">
+        <label for="city">Qayerda yashaysiz ?</label>
+        <select id="city" class="citySelect">
+          <option value="tosh">Toshkent</option>
+          <option value="sam">Samarqand</option>
+          <option value="sir">Sirdaryo</option>
+        </select>
+      </div>
+
+      <div class="form-control">
+        <label for="">Tokioga borishni hohlaysizmi ?</label>
+
+        <label for="" class="radio">
+          <input type="radio" id="yes" name="radio">
+          <span></span>
+          Ha
+        </label>
+        <label for="" class="radio">
+          <input type="radio" id="no" name="radio">
+          <span></span>
+          Yo'q
+        </label>
+      </div>
+
+      <div class="form-control">
+        <label>Vue da nimalarni bilasiz</label>
+
+        <label class="checkbox">
+          <input type="checkbox" id="vuex">
+          <span></span>
+          Vuex
+        </label>
+
+        <label class="checkbox">
+          <input type="checkbox" id="router">
+          <span></span>
+          Vue Router
+        </label>
+
+      </div>
+
+      <div type="submit" class="btn">Yuborish</div>
     </form>
   </div>
 </template>

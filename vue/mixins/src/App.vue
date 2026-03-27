38084da21651c906d4801c2b1bbb03a8ae6ea56f
jurabekkamolovdev <jurabek.dev101@gmail.com>
2026-03-27 21:18:19 +0500

    [+]

---
+++ vue/mixins/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <AppAlert
+      v-if="isAlertVisible"
+      title="Xato!"
+      text="Habarda xatolik bor"
+      type="danger"
+      @close="isAlertVisible = false"
+    ></AppAlert>
+    <div class="card">
+      <button class="btn" @click="toggleAlert">Habarni ko'rasatish</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppAlert from './components/AppAlert.vue'
+
+export default {
+  data() {
+    return {
+      isAlertVisible: false
+    }
+  },
+
+  methods: {
+    toggleAlert() {
+      this.isAlertVisible = !this.isAlertVisible
+    }
+  },
+
+  components: {
+    AppAlert
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ vue/mixins/src/components/AppAlert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alert" :class="type">
+    <h2>{{ title }}</h2>
+    <h3>{{ text }}</h3>
+    <button class="btn" :class="type" @click="$emit('close')">Yopish</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    text: String,
+    type: {
+      type: String,
+      default: 'primary'
+    }
+  }
+}
+</script>
